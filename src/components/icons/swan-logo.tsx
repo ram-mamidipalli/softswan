07003d6573474021d8cba24b3a83d27692a89f09
RAMA SUBBARAO <m.ramasubbarao17@gmail.com>
2025-09-19 10:15:35 +0000

    remove the icon from logo and make the SoftSwan as logo and Swan must be

--- src/components/icons/swan-logo.tsx
+++ src/components/icons/swan-logo.tsx
@@ -1,14 +1,9 @@
 import type { SVGProps } from 'react';
 
-export function SwanLogo(props: SVGProps<SVGSVGElement>) {
+export function SwanLogo(props: SVGProps<HTMLSpanElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      {...props}
-    >
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z M9.29 15.71L12 13l2.71 2.71c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 11.59l2.71-2.71c.39-.39.39-1.02 0-1.41s-1.02-.39-1.41 0L12 10.18l-2.71-2.71c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l2.71 2.71-2.71 2.71c-.39.39-.39 1.02 0 1.41.39.39 1.03.39 1.41 0z" />
-    </svg>
+    <span {...props}>
+      Soft<span className="text-primary">Swan</span>
+    </span>
   );
 }
